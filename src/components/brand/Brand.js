06d3b4ef34effa12a71d59a38a5502e0f5414933
kmwangemi <mwangemik@gmail.com>
2022-01-22 20:53:49 +0300

    add page components

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,9 +1,31 @@
+import {
+   google,
+   slack,
+   atlassian,
+   dropbox,
+   shopify
+} from './imports'
+
 import './brand.css'
 
 const Brand = () => {
    return (
-      <div>
-         Brand
+      <div className='gpt3__brand section__padding'>
+         <div>
+            <img src={google} loading='lazy' alt="google" />
+         </div>
+         <div>
+            <img src={slack} loading='lazy' alt="slack" />
+         </div>
+         <div>
+            <img src={atlassian} loading='lazy' alt="atlassian" />
+         </div>
+         <div>
+            <img src={dropbox} loading='lazy' alt="dropbox" />
+         </div>
+         <div>
+            <img src={shopify} loading='lazy' alt="shopify" />
+         </div>
       </div>
    )
 }
